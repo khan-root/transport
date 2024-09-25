cd6eaec27459eb3aeae04bb502fdf09282d90e94
Sarmad Faizan <sarmadfaizan@Sarmads-MacBook-Air.local>
2024-09-26 03:16:00 +0500

    change in price

--- utils/dataUtils.js
+++ utils/dataUtils.js
@@ -1,4 +1,4 @@
-import { FaEnvelope, FaFacebook, FaFacebookMessenger, FaLinkedin, FaLocationArrow, FaPhone, FaTwitter, FaWhatsapp } from "react-icons/fa6"
+import { FaFacebook, FaFacebookMessenger, FaLocationArrow, FaPhone, FaWhatsapp } from "react-icons/fa6"
 import { IoLogoWechat } from "react-icons/io5"
 
 export const navabrData = [
@@ -49,13 +49,13 @@ export const constReachOutData = [
 
 export const categoryData = {
   cars: [
-    {id:1, price:500, rangePrice: 850, name:'Mercedes V Class ( 7-seater)', 
+    {id:1, price:'Negotiable', rangePrice: '', name:'Mercedes V Class ( 7-seater)', 
         ideal_seats:7, max_seat:'', class:'First Class',
         main_description:'Ideal for large group transportation, this coach offers comfortable seating for up to 7 passengers with daily, hourly, and monthly rental options.',
         tableData :[
-            {price:'8500 to 13000', title:'Monthly', time:'30 Days'},	
-            {price:'500 to 850', title:'Per Day', time:'10 Hrs'},	
-            {price:'100', title:'Per Hour', time:'1Hr'},	
+            {price:'Negotiable', title:'Monthly', time:'30 Days'},	
+            {price:'Negotiable', title:'Per Day', time:'10 Hrs'},	
+            {price:'Negotiable', title:'Per Hour', time:'1Hr'},	
 
         ],
         tableHeader:['Car Type', 'Time', 'Price'],
@@ -150,13 +150,13 @@ export const categoryData = {
     },
   ],
   buses: [
-    {id:1, price:1000, rangePrice: 1500, name:'51-Seater-Luxury-Buses', 
+    {id:1, price:'Negotiable', rangePrice: '', name:'51-Seater-Luxury-Buses', 
         ideal_seats:51, max_seat:'', class:'First Class',
         main_description:'Ideal for large group transportation, this coach offers comfortable seating for up to 51 passengers with daily, hourly, and monthly rental options.',
         tableData :[
-            {price:'18000 to 24000', title:'Monthly', time:'30 Days'},	
-            {price:'1000 to 1500', title:'Per Day', time:'10 Hrs'},	
-            {price:'200', title:'Per Hour', time:'1Hr'},	
+            {price:'Negotiable', title:'Monthly', time:'30 Days'},	
+            {price:'Negotiable', title:'Per Day', time:'10 Hrs'},	
+            {price:'Negotiable', title:'Per Hour', time:'1Hr'},	
 
         ],
         tableHeader:['Bus Type', 'Time', 'Price'],
@@ -167,13 +167,13 @@ export const categoryData = {
         ],
         btnName:'Bus'
     },
-    {id:2, price:700, rangePrice: 1200, name:'35 Seater Yutong Luxury-Buses', 
+    {id:2, price:'Negotiable', rangePrice: '', name:'35 Seater Yutong Luxury-Buses', 
         ideal_seats:35, max_seat:'', class:'First Class',
         main_description:'Ideal for large group transportation, this coach offers comfortable seating for up to 35 passengers with daily, hourly, and monthly rental options.',
         tableData :[
-            {price:'16000 to 21000', title:'Monthly', time:'30 Days'},	
-            {price:'700 to 1200', title:'Per Day', time:'10 Hrs'},	
-            {price:'150', title:'Per Hour', time:'1Hr'},	
+            {price:'Negotiable', title:'Monthly', time:'30 Days'},	
+            {price:'Negotiable', title:'Per Day', time:'10 Hrs'},	
+            {price:'Negotiable', title:'Per Hour', time:'1Hr'},	
 
         ],
         tableHeader:['Bus Type', 'Time', 'Price'],
@@ -210,13 +210,13 @@ export const categoryData = {
     //     tableDesctription:'The rental cost of a 51-Seater Charter Coach in Dubai is as follows',
     //     image:'/assets/bus3.jpg'
     // },
-    {id:5, price:650, rangePrice: 1100, name:'22-Seater Coaster', 
+    {id:5, price:'Negotiable', rangePrice: '', name:'22-Seater Coaster', 
         ideal_seats:22, max_seat:'', class:'First Class',
         main_description:'Ideal for large group transportation, this coach offers comfortable seating for up to 22 passengers with daily, hourly, and monthly rental options.',
         tableData :[
-             {price:'10000 to 16000', title:'Monthly', time:'30 Days'},	
-            {price:'650 to 1100', title:'Per Day', time:'10 Hrs'},	
-            {price:'125', title:'Per Hour', time:'1Hr'},	
+             {price:'Negotiable', title:'Monthly', time:'30 Days'},	
+            {price:'Negotiable', title:'Per Day', time:'10 Hrs'},	
+            {price:'Negotiable', title:'Per Hour', time:'1Hr'},	
 
         ],
         tableHeader:['Bus Type', 'Time', 'Price'],
@@ -248,13 +248,13 @@ export const categoryData = {
         ],
         btnName:'Hiace'
     },
-    {id:2, price:850, rangePrice: '', name:'15-Seater Hiace (HiRoof)', 
+    {id:2, price:'Negotiable', rangePrice: '', name:'15-Seater Hiace (HiRoof)', 
         ideal_seats:15, max_seat:'', class:'First Class',
         main_description:'Ideal for large group transportation, this coach offers comfortable seating for up to 51 passengers with daily, hourly, and monthly rental options.',
         tableData :[
-            {price:'15000 to 25000', title:'Monthly', time:'30 Days'},	
-            {price:'850', title:'Per Day', time:'10 Hrs'},	
-            {price:'70', title:'Per Hour', time:'1Hr'},	
+            {price:'Negotiable', title:'Monthly', time:'30 Days'},	
+            {price:'Negotiable', title:'Per Day', time:'10 Hrs'},	
+            {price:'Negotiable', title:'Per Hour', time:'1Hr'},	
 
         ],
         tableHeader:['Hiace Type', 'Time', 'Price'],
